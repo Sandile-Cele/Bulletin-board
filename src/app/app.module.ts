4e75b34d7321e72c10f1b657560b8304f77d72ea
Sandile-Cele <Sandilecele101@gmail.com>
2021-03-25 09:17:31 +0200

    + order component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderCreateComponent } from './order-create/order-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderCreateComponent
   ],
   imports: [
     BrowserModule
