3c816abfe25e28d54ce794f7f2f90f3338bf876f
Sandile-Cele <Sandilecele101@gmail.com>
2021-03-28 21:49:40 +0200

    broken theme setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { OrderCreateComponent } from './order-create/order-create.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { OrderCreateComponent } from './order-create/order-create.component';
   ],
   imports: [
     BrowserModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      // MatInputModule,
+      // MatCardModule,
+      // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
