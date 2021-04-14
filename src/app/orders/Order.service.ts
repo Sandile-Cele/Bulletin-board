c6add7f49f142c6b40956dea1498deae9fec66a0
Sandile-Cele <Sandilecele101@gmail.com>
2021-04-14 19:07:22 +0200

    broken observable

--- src/app/orders/Order.service.ts
+++ src/app/orders/Order.service.ts
@@ -1,21 +1,24 @@
 import {Order} from './Order.model';
 import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-
 export class OrderService{
-  private order: Order [] = [];
-
-  getOrders(){
-    const testing: Order = {userName: "Sandile", email:"sandfasfd@gmaldf.com", orderDec: "asdf asdf"}
-    this.order.push(testing);
-    return [...this.order];
-  }
+  private orders: Order [] = [];
+  private updatedOrders = new Subject<Order>();
 
   setOrders(inOrder: Order){
-
-    this.order.push(inOrder);
+    this.orders.push(inOrder);
   }
 
+  getOrders(){
+    const testing: Order = {userName: "Sandile1", email:"sandfasfd@gmaldf.com", orderDec: "asdf asdf"}
+    this.orders.push(testing);
+    return [this.orders];
+  }
+
+  getPostUpdateLister(){
+    return this.updatedOrders.asObservable();
+  }
 
 }
--- src/app/orders/order-placed/order-placed.component.ts
+++ src/app/orders/order-placed/order-placed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Order } from '../Order.model';
 import { OrderService } from '../order.service';
 
@@ -10,12 +11,20 @@ import { OrderService } from '../order.service';
 
 export class OrderPlacedComponent implements OnInit, OnDestroy {
   orders:Order[] = [];//@Input()
+  private orderSubscription: Subscription;
 
   constructor(public orderService: OrderService) {}
 
   ngOnInit(){
     this.orders = this.orderService.getOrders();
+    // this.orderSubscription = this.orderService.getPostUpdateLister()
+    //   .subscribe((orders: Order[]) =>
+    //   {
+    //      this.orders = orders
+    //   });
+
   }
+
   ngOnDestroy(){
 
   }
