82c07dfc4ee605dc6974074ba98580a53f89ad83
Sandile-Cele <Sandilecele101@gmail.com>
2021-04-16 17:43:00 +0200

    can get fake orders from server

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send(
-    "I'm the first middle ware, if I use next I will call the second, if not nada will happen"
-  );
-  next();
-});
+app.use('/api/orders', (req, res, next) => {
 
-app.use((req, res, next) => {
-  console.log(
-    "This response is being sent by middle ware called by" +
-      "the previous middle ware using next"
-  );
-});
+  const orders = [
+    {
+      id: '1',
+      userName: 'sandileCele',
+      email: 'sandile@email.com',
+      orderDec: 'Cheese cake from server'
+    },
+    {
+      id: '2',
+      userName: 'Molly jones',
+      email: 'Mollyjones@email.com',
+      orderDec: 'Lemon pie cake from server'
+    }
+  ]
+    res.json({ messages: 'Orders retrieved from server successfully', orders: orders });
 
-module.exports = app;
+  });
+
+
+  //   res.send(
+  //     "I'm the first middle ware, if I use next I will call the second, if not nada will happen"
+  //   );
+  //   next();
+  // });
+
+  app.use((req, res, next) => {
+    console.log(
+      "This response is being sent by middle ware called by \nthe previous middle ware using next"
+
+    );
+  });
+
+  module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const app = require('./backend/app');
+const http = require("http");
+const app = require("./backend/app");
 
-const port = (process.env.PORT || "3000");
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-app.set('port', port);
+app.set("port", port);
 
 server.listen(port);
