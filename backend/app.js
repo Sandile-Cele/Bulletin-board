2c06ee3aad65cd19d128387b8d508b5bf69b0301
Sandile-Cele <Sandilecele101@gmail.com>
2021-05-24 05:33:36 +0200

    Revert "accept user input through post"
    This reverts commit 76c24adf557cc378640e87e1d88b014126dca0d6.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
-console.log("server has started");
+console.log('server has started');
 const express = require("express");
+<<<<<<< HEAD
 // const bodyParser = require("body-parser"); //This deprecated
+=======
+const bodyParser = require('body-parser');
+
+>>>>>>> parent of 76c24ad (accept user input through post)
 const app = express();
 const order = require("./model/order");
 const mongoose = require("mongoose");
@@ -14,16 +19,15 @@ const options = {
 app.use(express.json()); //Because app.use(bodyParser.json());...
 app.use(express.urlencoded());//...is deprecated
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
-  );
+app.use((req, res, next) =>
+{
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
   res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 });
 
+<<<<<<< HEAD
 app.get("/api/orders", (req, res, next) =>
 {
   order.find().then((documents)=>
@@ -47,10 +51,16 @@ app.use("/api/orders", (req, res, next) => {
   orders.save();
 
   console.log("order created:" + orders);
+=======
+app.use('/api/orders', (req, res, next) => {
+>>>>>>> parent of 76c24ad (accept user input through post)
 
+  const orders = req.body;
+  // console.log(orders);
   res.status(201).json({
-    messages: "Post successfully created!",
+    messages: 'order successfully created!'
   });
+<<<<<<< HEAD
 });
 
 app.use((req, res, next) => {
@@ -58,26 +68,34 @@ app.use((req, res, next) => {
     "Backend done!"
   );
 });
+=======
+>>>>>>> parent of 76c24ad (accept user input through post)
 
-// app.use("/api/orders", orderRoutes)
-// app.use("/api/user", userRoutes)
-module.exports = app;
+  // orders = [
+    //   {
+      //     id: '1',
+      //     userName: 'sandileCele',
+      //     email: 'sandile@email.com',
+      //     orderDec: 'Cheese cake from server'
+      //   },
+      //   {
+        //     id: '2',
+        //     userName: 'Molly jones',
+        //     email: 'Mollyjones@email.com',
+        //     orderDec: 'Lemon pie cake from server'
+        //   }
+        // ]
+        //  res.json({ messages: 'Orders retrieved from server successfully', orders: orders });
 
-//This is code post orders from server to app
-/*
-  orders = [
-    {
-      id: '1',
-      userName: 'sandileCele',
-      email: 'sandile@email.com',
-      orderDec: 'Cheese cake from server'
-    },
-    {
-      id: '2',
-      userName: 'Molly jones',
-      email: 'Mollyjones@email.com',
-      orderDec: 'Lemon pie cake from server'
-    }
-  ]
-   res.json({ messages: 'Orders retrieved from server successfully', orders: orders });
-*/
+      });
+
+      app.use((req, res, next) => {
+        console.log(
+          "This response is being sent by middle ware called by \nthe previous middle ware using next"
+
+        );
+      });
+
+      // app.use("/api/orders", orderRoutes)
+      // app.use("/api/user", userRoutes)
+      module.exports = app
