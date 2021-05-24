b4c49cfbc9a8f5196b4fd82e0518772033110bfe
Sandile-Cele <Sandilecele101@gmail.com>
2021-05-24 05:48:15 +0200

    Revert "connected to mongoDb"
    This reverts commit 31d5d903a2a2efbacb12c335cda3444d555c6aab.

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,7 @@
-console.log('server has started');
+console.log("server has started");
 const express = require("express");
 <<<<<<< HEAD
 // const bodyParser = require("body-parser"); //This deprecated
-=======
-const bodyParser = require('body-parser');
-
->>>>>>> parent of 76c24ad (accept user input through post)
 const app = express();
 const order = require("./model/order");
 const mongoose = require("mongoose");
@@ -18,16 +14,25 @@ const options = {
 
 app.use(express.json()); //Because app.use(bodyParser.json());...
 app.use(express.urlencoded());//...is deprecated
+=======
+const bodyParser = require("body-parser");
+const Order = require("./model/order");
 
-app.use((req, res, next) =>
-{
-  res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+const app = express();
+
+app.use(bodyParser.json());
+>>>>>>> parent of 31d5d90 (connected to mongoDb)
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
   res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 });
 
-<<<<<<< HEAD
 app.get("/api/orders", (req, res, next) =>
 {
   order.find().then((documents)=>
@@ -51,51 +56,41 @@ app.use("/api/orders", (req, res, next) => {
   orders.save();
 
   console.log("order created:" + orders);
-=======
-app.use('/api/orders', (req, res, next) => {
->>>>>>> parent of 76c24ad (accept user input through post)
 
-  const orders = req.body;
-  // console.log(orders);
   res.status(201).json({
-    messages: 'order successfully created!'
+    messages: "Post successfully created!",
   });
-<<<<<<< HEAD
 });
 
 app.use((req, res, next) => {
   console.log(
+<<<<<<< HEAD
     "Backend done!"
+=======
+    "This response is being sent by middle ware called by \nthe previous middle ware using next"
+>>>>>>> parent of 31d5d90 (connected to mongoDb)
   );
 });
-=======
->>>>>>> parent of 76c24ad (accept user input through post)
 
-  // orders = [
-    //   {
-      //     id: '1',
-      //     userName: 'sandileCele',
-      //     email: 'sandile@email.com',
-      //     orderDec: 'Cheese cake from server'
-      //   },
-      //   {
-        //     id: '2',
-        //     userName: 'Molly jones',
-        //     email: 'Mollyjones@email.com',
-        //     orderDec: 'Lemon pie cake from server'
-        //   }
-        // ]
-        //  res.json({ messages: 'Orders retrieved from server successfully', orders: orders });
+// app.use("/api/orders", orderRoutes)
+// app.use("/api/user", userRoutes)
+module.exports = app;
 
-      });
-
-      app.use((req, res, next) => {
-        console.log(
-          "This response is being sent by middle ware called by \nthe previous middle ware using next"
-
-        );
-      });
-
-      // app.use("/api/orders", orderRoutes)
-      // app.use("/api/user", userRoutes)
-      module.exports = app
+//This is code post orders from server to app
+/*
+  orders = [
+    {
+      id: '1',
+      userName: 'sandileCele',
+      email: 'sandile@email.com',
+      orderDec: 'Cheese cake from server'
+    },
+    {
+      id: '2',
+      userName: 'Molly jones',
+      email: 'Mollyjones@email.com',
+      orderDec: 'Lemon pie cake from server'
+    }
+  ]
+   res.json({ messages: 'Orders retrieved from server successfully', orders: orders });
+*/
