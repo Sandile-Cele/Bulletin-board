b849633e24e7dabcf2e7ad209b85a1847be8d106
Sandile-Cele <Sandilecele101@gmail.com>
2021-05-27 05:41:25 +0200

    !!! Login& signup, + routing

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  enteredUserNameError = 'Please enter a username in the correct form!';
   enteredEmailError = 'Please enter a correctly formatted e-mail address!';
   enteredPasswordError = 'Please enter a password that contains lower case, upper case letters and at least one number!';
-  enteredPasswordError2 = 'Make sure passwords match and they meet requirement for the first password!';
 
   constructor() { }
 
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  enteredUserNameError = 'Please enter a username in the correct form!';
+  enteredEmailError = 'Please enter a correctly formatted e-mail address!';
+  enteredPasswordError = 'Please enter a password that contains lower case, upper case letters and at least one number!';
+  enteredPasswordError2 = 'Make sure passwords match and they meet requirement for the first password!';
+
   constructor() { }
 
   ngOnInit(): void {
