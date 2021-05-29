70699438c0f78b3d75809877b4326d678109cbd1
Sandile-Cele <Sandilecele101@gmail.com>
2021-05-29 03:17:47 +0200

    ++signup, role added

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { UserData } from '../auth.model';
 
 @Component({
   selector: 'app-signup',
@@ -10,17 +12,31 @@ export class SignupComponent implements OnInit {
 
   enteredUserNameError = 'Please enter a username in the correct form!';
   enteredEmailError = 'Please enter a correctly formatted e-mail address!';
+  enteredRoleError = 'Role entered must be >2 and <11';
   enteredPasswordError = 'Please enter a password that contains lower case, upper case letters and at least one number!';
   enteredPasswordError2 = 'Make sure passwords match and they meet requirement for the first password!';
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
-  onSignup(form: NgForm){
-    console.log(form.value);
+  ngOnInit(): void {}
+
+  onSignup(signupForm: NgForm){
+
+    if(signupForm.invalid){
+      return;
+    }
+
+    var newUser: UserData = {
+      username: signupForm.value.inUsername,
+      email: signupForm.value.inEmail,
+      password: signupForm.value.password,
+      role: signupForm.value.role
+    };
+
+    this.authService.createUser(newUser);
+  }
 
   }
 
-  ngOnInit(): void {
-  }
 
 }
