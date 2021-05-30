44936295b4eb3899dda0f2759e57f51ab09f794f
Sandile-Cele <Sandilecele101@gmail.com>
2021-05-30 02:53:26 +0200

    ++mongoose-unique-validator install

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const User = require("../model/user");
 // const jwt = require("jsonwebtoken");
 // const ExpressBrute = require("express-brute");
+//const store = new ExpressBrute.MemoryStore();
 // const bruteforce = new ExpressBrute(store);
 
 router.post("/signup", (req, res, next) => {
@@ -15,8 +16,12 @@ router.post("/signup", (req, res, next) => {
       password: hash,
     });
 
-    console.log("From request:"+req.body.password, req.body.email, req.body.username);
-    console.log("From being saved:"+newUser);
+    console.log(
+      "From request:" + req.body.password,
+      req.body.email,
+      req.body.username
+    );
+    console.log("From being saved:" + newUser);
 
     newUser.save();
 
@@ -40,4 +45,49 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
-module.exports= router;
+router.post("/login", bruteforce.prevent, (req, res, next) => {
+  // creating global user variable to use in different code blocks.
+  let fetchedUser;
+  //checks if we have user with a valid e-mail address
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      console.log(user);
+      if (!user) {
+        return res.status(401).json({
+          message: "Authentication Failed, try again ",
+        });
+      }
+      // assigning retrieved user to global variable so we can use him later
+      fetchedUser = user;
+      //compares hashed passwords (alwasy the same hash with same input)
+      return bcrypt.compare(req.body.password, user.password); // compare returned user password and password in db
+    })
+    .then((result) => {
+      console.log("2", result);
+      if (!result) {
+        return res.status(401).json({
+          message: "Authentication Failure ",
+        });
+      }
+      //create JWT if user exists : JWT contains user e mail and user ID from user object
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer_time_is",
+        {
+          expiresIn: "1h",
+        }
+      );
+      console.log(token);
+      res.status(200).json({
+        token: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(401).json({
+        message: "Authentication Failure",
+      });
+    });
+});
+
+module.exports = router;
