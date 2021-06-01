1172a4e4384a37a8bd9bfd7127ce6f7f1dc77f96
Sandile-Cele <Sandilecele101@gmail.com>
2021-06-01 14:30:41 +0200

    !Bruteforce plus README

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
-// const ExpressBrute = require("express-brute");
-//const store = new ExpressBrute.MemoryStore();
-// const bruteforce = new ExpressBrute(store);
+const ExpressBrute = require("express-brute");
+const store = new ExpressBrute.MemoryStore();
+const bruteforce = new ExpressBrute(store);
 
+//Password is being hashed and salted
+//JWT token is used
 router.post("/signup", (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const newUser = new User({
@@ -28,28 +30,12 @@ router.post("/signup", (req, res, next) => {
     res.status(201).json({
       message: "User Successfully created(FROM BACKEND)",
     });
-
-                          //Try and use this
-                          // newUser.save()
-                          //   .then((result) => {
-                          //     res.status(201).json({
-                          //       message: "User created!",
-                          //       result: result,
-                          //     });
-                          //   })
-                          //   .catch((err) => {
-                          //     res.status(500).json({
-                          //       error: err,
-                          //     });
-                          //   });
   });
 });
 
-router.post("/login", (req, res, next) => {
-  // creating global user variable to use in different code blocks.
+router.post("/login", bruteforce.prevent, (req, res, next) => {
+
   let fetchedUser;
-  //checks if we have user with a valid e-mail address
-  console.log(req.body);
 
   User.findOne({ email: req.body.email })
     .then((user) => {
@@ -59,10 +45,10 @@ router.post("/login", (req, res, next) => {
           message: "Authentication Failed, try again ",
         });
       }
-      // assigning retrieved user to global variable so we can use him later
+
       fetchedUser = user;
-      //compares hashed passwords (always the same hash with same input)
-      return bcrypt.compare(req.body.password, user.password); // compare returned user password and password in db
+
+      return bcrypt.compare(req.body.password, user.password);
     })
     .then((result) => {
       console.log("2", result);
@@ -71,7 +57,7 @@ router.post("/login", (req, res, next) => {
           message: "Authentication Failure ",
         });
       }
-      //create JWT if user exists : JWT contains user e mail and user ID from user object
+
       const token = jwt.sign(
         { email: fetchedUser.email, userId: fetchedUser._id, username: fetchedUser.username},
           "my_super_secret",
---
+++ src/app/README.txt
@@ -0,0 +1,4 @@
+To run servers with TLS:
+Frontend: ng serve --ssl true --ssl-cert "Keys\certificate.pem" --ssl-key "Keys\privatekey.pem"
+Backend: npm run start:server
+
